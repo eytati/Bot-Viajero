40a4c30e92674fa6ec62a885b133915fdedf4f2e
Tati <eytati050@hotmail.com>
2017-03-17 16:22:42 -0600

    Json y lecturas de json
    Se agrego un proceso de autenticacion de oauth 2.0
    Se agregaron los json de nodos y aristas
    Se agrego la lectura de los nodos y las aristas

--- Bot-Viajero-Back/Modulos/Autenticacion.py
+++ Bot-Viajero-Back/Modulos/Autenticacion.py
@@ -1,3 +1,5 @@
+from oauth2client.client import flow_from_clientsecrets
+'''
 from requests.auth import HTTPBasicAuth
 
 
@@ -8,3 +10,9 @@ requests.get('https://api.github.com/user', auth=('user', 'pass'))
 url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET', 'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
 requests.get(url, auth=auth)
+
+'''
+
+low = flow_from_clientsecrets('path_to_directory/client_secrets.json',
+                               scope='https://www.googleapis.com/auth/calendar',
+                               redirect_uri='http://example.com/auth_return')
--- Bot-Viajero-Back/Modulos/Grafo.py
+++ Bot-Viajero-Back/Modulos/Grafo.py
@@ -1,12 +1,34 @@
 import networkx as nx
+import json
 
-datos1 = {
-    "Id": 0,
-    "longitud": 0.0,
-    "latitud": 0.0,
-    "nombre": ''
-}
+
+class creacion_de_grafo:
+
+
+    def carga_de_nodos(self):
+        nodos = []
+        with open('data.json') as datos_json:
+            nodos = json.load(datos_json)
+            print(nodos)
+        '''
+        for contador in range(len (nodos["locations"])):
+                grafo.add_node(contador, nodos[contador])
+        print(str(grafo))
+        '''
+
+    def carga_de_aristas(self):
+        aristas = []
+        with open('edges.json') as datos_json:
+            aristas = json.load(datos_json)
+            print(aristas)
+
 grafo = nx.Graph()
+a = creacion_de_grafo()
+a.carga_de_nodos()
+a.carga_de_aristas()
+
+
+'''
 grafo.add_node(datos1.get("Id"),attr_dict=datos1)
 grafo.add_edge(1,2, weight=0.9)
 grafo.add_edge(1,2, weight=0.9)
@@ -15,7 +37,7 @@ grafo.add_edge(1,2, weight=0.9)
 nx.dijkstra_path(grafo,1,2)
 
 
-
+'''
 
 
 
