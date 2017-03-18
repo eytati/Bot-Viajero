d70a53dca3dad50b1f33ccc45147471353001252
Tati <eytati050@hotmail.com>
2017-03-18 02:06:43 -0600

    Lecturas de rutas
    Se agrego la lectura de rutas desde el Json y lo carga en un grafo

--- Bot-Viajero-Back/Modulos/Grafo.py
+++ Bot-Viajero-Back/Modulos/Grafo.py
@@ -5,27 +5,30 @@ import json
 class creacion_de_grafo:
 
 
-    def carga_de_nodos(self):
+    def carga_de_nodos(self,grafo):
         nodos = []
         with open('data.json') as datos_json:
             nodos = json.load(datos_json)
-            print(nodos)
-        '''
-        for contador in range(len (nodos["locations"])):
-                grafo.add_node(contador, nodos[contador])
-        print(str(grafo))
-        '''
+           # print(nodos)
 
-    def carga_de_aristas(self):
+            for contador in range(len (nodos["locations"])):
+                grafo.add_node(nodos["locations"][contador].get('id'), nodos["locations"][contador])
+        print(grafo)
+
+    def carga_de_aristas(self,grafo):
         aristas = []
         with open('edges.json') as datos_json:
             aristas = json.load(datos_json)
-            print(aristas)
+        for contador in range(len(aristas["routes"])):
+            grafo.add_edge(aristas['routes'][contador].get('a'),aristas['routes'][contador].get('b'), weight= aristas['routes'][contador].get('weight'))
+           # grafo.add_node(aristas["routes"][contador].get('a'),aristas["routes"][contador].get('b'), weight= aristas["routes"][contador].get('weight') )
 
 grafo = nx.Graph()
 a = creacion_de_grafo()
-a.carga_de_nodos()
-a.carga_de_aristas()
+a.carga_de_nodos(grafo)
+a.carga_de_aristas(grafo)
+print(grafo)
+#a.carga_de_aristas()
 
 
 '''
