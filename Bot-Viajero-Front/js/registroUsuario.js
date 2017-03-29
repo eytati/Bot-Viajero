cc144b345ee8a9510a2e9e92743d08b245281d7f
Tati <eytati050@hotmail.com>
2017-03-28 19:43:19 -0600

    prueba de JS en y  utilizacion en HTML

--- Bot-Viajero-Front/js/registroUsuario.js
+++ Bot-Viajero-Front/js/registroUsuario.js
@@ -1,3 +1,25 @@
-/**
- * Created by hunterz on 3/23/17.
- */
+
+
+alert("Hola Mundo");
+
+function getMyData(data) {
+    alert(data);
+}
+
+getMyData("Enter your data here");
+
+var username = ana;
+var password = ana;
+var repassword = ana;
+
+var json_registro = {"username": username, "password": password, "repassword": repassword};
+alert2({
+            type: 'POST',
+            url: '127.0.0.1:5000/api/registrar/persona',
+            data: json_registro,
+            dataType: 'application/json',
+            success: function(data) {
+             alert("POSTED SUCCESSFULLY TO THE SERVER");
+             $('#subscribePage').html('<h1>POSTED</h1>');
+          } // Success Function
+          });
