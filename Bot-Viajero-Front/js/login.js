14bc753df59bb70692b59a8798c6c7952d5380aa
Pedro Guzman <siqsoftwarelabs@outlook.com>
2017-04-07 13:04:37 -0600

    Script Registro

---
+++ Bot-Viajero-Front/js/login.js
@@ -0,0 +1,3 @@
+/**
+ * Created by hunterz on 4/7/17.
+ */
--- Bot-Viajero-Front/js/registroUsuario.js
+++ Bot-Viajero-Front/js/registroUsuario.js
@@ -10,7 +10,7 @@ var username = document.getElementById('username');
 var password = document.getElementById('password');
 var confirmPassword = document.getElementById('confirmPassword');
 
-var json_registro = {"username": username, "password": password, "confirmPassword": confirmPassword};
+/*var json_registro = {"username": username, "password": password, "confirmPassword": confirmPassword};
 alert2({
             type: 'POST',
             url: '127.0.0.1:5000/api/registrar/persona',
@@ -20,8 +20,47 @@ alert2({
              alert("POSTED SUCCESSFULLY TO THE SERVER");
              $('#subscribePage').html('<h1>POSTED</h1>');
           } // Success Function
-          });
+          });*/
+var json_registro = {"username": username, "password": password, "confirmPassword": confirmPassword};
+    register = $.ajax({
+        type: 'POST',
+        url: '192.168.1.137:5000/api/registrar/persona',
+        data: json_registro,
+        success: function (response) {
+            output= response;
+            alert(output);
+        }
+    }).done(function (data) {
+        console.log(data);
+        alert(data);
+    });
 
+
+/* AJAX $(document).ready(function () {
+    $("#botonRegistroUsuario").click(function (e) {
+
+        var user_name= $('#username').val()
+        var password= $('#password').val()
+        var confirmPassword= $('#confirmPassword').val()
+        console.log(user_name);
+        console.log(password);
+        console.log(confirmPassword);
+
+        var user= user_name;
+        var pass= password;
+        var confirm= confirmPassword;
+
+        e.preventDefault()
+
+        $.ajax({
+            type: "POST",
+            url: '127.0.0.1:5000/api/registrar/persona',
+
+
+        })
+
+    })
+})* AJAX/
 function checkPassword() {
 
      if (password.value != confirmPassword.value){
